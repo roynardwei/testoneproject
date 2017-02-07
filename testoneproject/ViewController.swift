17ca74e287e28fa2884ed795df8fa9aeaf17a232
yui shangwei <mina@roynardsmacbook.local>
2017-02-07 19:41:25 +0800

    Initial Commit

---
+++ testoneproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testoneproject
+//
+//  Created by Roynard on 2017/2/7.
+//  Copyright © 2017年 Roynard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
